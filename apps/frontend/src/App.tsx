77fbc633cbb62eb0ad69d3297b0c2eed8d6404cb
dcoollx <dsqjr@msn.com>
2026-04-20 18:04:31 -0400

    launching cart

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,9 +2,10 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Home } from "./pages/Home";
 import { queryClient } from "./util/queryClient";
 import { CartProvider } from 'use-shopping-cart'
-import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router';
 import { Checkout } from "./pages/Checkout";
 import { useEffect } from "react";
+import { Success } from "./pages/Success";
 
 
 export default function App() {
@@ -15,6 +16,8 @@ export default function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/checkout" element={<Checkout />} />
+            <Route path="/success" element={<Success/>} />
+            <Route path="*" element={<Navigate to="/"/>} />
           </Routes>
         </Router>
       </CartProvider>
--- apps/frontend/src/components/ProductList.tsx
+++ apps/frontend/src/components/ProductList.tsx
@@ -16,13 +16,7 @@ import { Skeleton } from "./ui/skeleton";
     }
     return (
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
-        {[...products, {
-    "Product_Name": "Pink Sugar",
-    "Image": "https://plus.unsplash.com/premium_vector-1737035301774-79613c87d8bb?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "Unit_Price": 5.00,
-    "Description": "2 Oz",
-    "id": "7374418000000609049"
-}].map((product: ZohoProduct) => (
+        {products.map((product: ZohoProduct) => (
         <a role="button"  key={product.id} onClick={(_)=>{
           addItem({
             id: product.id,
